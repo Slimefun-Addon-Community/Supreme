24e35a3098f983cfa12fca46b9a1c6940d7b2b15
Ricardo <ricardo.relativo@gmail.com>
2022-08-06 00:51:30 -0300

    fix ms generator and electric craft

--- src/main/java/com/github/relativobr/supreme/machine/ElectricCrafter.java
+++ src/main/java/com/github/relativobr/supreme/machine/ElectricCrafter.java
@@ -1,6 +1,6 @@
 package com.github.relativobr.supreme.machine;
 
-import com.github.relativobr.machine.MediumContainerMachine;
+import com.github.relativobr.machine.FlexItemContainerMachine;
 import com.github.relativobr.recipe.AbstractItemRecipe;
 import com.github.relativobr.supreme.resource.SupremeComponents;
 import com.github.relativobr.supreme.resource.magical.SupremeCetrus;
@@ -18,9 +18,7 @@ import java.util.List;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
-
-public class ElectricCrafter extends MediumContainerMachine {
-
+public class ElectricCrafter extends FlexItemContainerMachine {
 
   public static final SlimefunItemStack ELECTRIC_CRAFTER_MACHINE = new SupremeItemStack("SUPREME_ELECTRIC_CRAFTER_I",
       Material.CRAFTING_TABLE, "&bElectric Crafter", "", "&fCraft Items", "",
@@ -49,40 +47,42 @@ public class ElectricCrafter extends MediumContainerMachine {
       ElectricCrafter.ELECTRIC_CRAFTER_MACHINE_II, SupremeComponents.SUPREME, SupremeComponents.CRYSTALLIZER_MACHINE,
       SupremeCetrus.CETRUS_LUMIUM, SupremeComponents.CRYSTALLIZER_MACHINE};
 
-  public static final AbstractItemRecipe RECIPE_BATTERY = new AbstractItemRecipe(new SlimefunItemStack(SlimefunItems.BATTERY, 1),
+  public static final AbstractItemRecipe RECIPE_BATTERY = new AbstractItemRecipe(
       new ItemStack[]{new ItemStack(SlimefunItems.ZINC_INGOT), new ItemStack(SlimefunItems.SULFATE),
           new ItemStack(SlimefunItems.COPPER_INGOT), new ItemStack(SlimefunItems.ZINC_INGOT),
           new ItemStack(SlimefunItems.SULFATE), new ItemStack(SlimefunItems.COPPER_INGOT),
-          new ItemStack(Material.REDSTONE), null, null});
-  public static final AbstractItemRecipe RECIPE_BLAZE_POWDER = new AbstractItemRecipe(new ItemStack(Material.BLAZE_POWDER, 2),
-      new ItemStack[]{new ItemStack(Material.BLAZE_ROD), null, null, null, null, null, null, null, null});
-  public static final AbstractItemRecipe RECIPE_ENDER_EYE = new AbstractItemRecipe(new ItemStack(Material.ENDER_EYE, 1),
+          new ItemStack(Material.REDSTONE), null, null}, new SlimefunItemStack(SlimefunItems.BATTERY, 1));
+  public static final AbstractItemRecipe RECIPE_BLAZE_POWDER = new AbstractItemRecipe(
+      new ItemStack[]{new ItemStack(Material.BLAZE_ROD), null, null, null, null, null, null, null, null},
+      new ItemStack(Material.BLAZE_POWDER, 2));
+  public static final AbstractItemRecipe RECIPE_ENDER_EYE = new AbstractItemRecipe(
       new ItemStack[]{new ItemStack(Material.BLAZE_POWDER), new ItemStack(Material.ENDER_PEARL), null, null, null, null,
-          null, null, null});
-  public static final AbstractItemRecipe RECIPE_GLASS_BOTTLE = new AbstractItemRecipe(new ItemStack(Material.GLASS_BOTTLE, 3),
+          null, null, null}, new ItemStack(Material.ENDER_EYE, 1));
+  public static final AbstractItemRecipe RECIPE_GLASS_BOTTLE = new AbstractItemRecipe(
       new ItemStack[]{new ItemStack(Material.GLASS), new ItemStack(Material.GLASS), new ItemStack(Material.GLASS), null,
-          null, null, null, null, null});
-  public static final AbstractItemRecipe RECIPE_FISHING_ROD = new AbstractItemRecipe(new ItemStack(Material.FISHING_ROD, 1),
+          null, null, null, null, null}, new ItemStack(Material.GLASS_BOTTLE, 3));
+  public static final AbstractItemRecipe RECIPE_FISHING_ROD = new AbstractItemRecipe(
       new ItemStack[]{new ItemStack(Material.STICK), new ItemStack(Material.STRING), new ItemStack(Material.STRING),
-          new ItemStack(Material.STICK), null, null, null, null, null});
-  public static final AbstractItemRecipe RECIPE_FLINT_AND_STEEL = new AbstractItemRecipe(new ItemStack(Material.FLINT_AND_STEEL, 1),
+          new ItemStack(Material.STICK), null, null, null, null, null}, new ItemStack(Material.FISHING_ROD, 1));
+  public static final AbstractItemRecipe RECIPE_FLINT_AND_STEEL = new AbstractItemRecipe(
       new ItemStack[]{new ItemStack(Material.IRON_INGOT), new ItemStack(Material.FLINT), null, null, null, null, null,
-          null, null});
-  public static final AbstractItemRecipe RECIPE_SHEARS = new AbstractItemRecipe(new ItemStack(Material.SHEARS, 1),
+          null, null}, new ItemStack(Material.FLINT_AND_STEEL, 1));
+  public static final AbstractItemRecipe RECIPE_SHEARS = new AbstractItemRecipe(
       new ItemStack[]{new ItemStack(Material.IRON_INGOT), new ItemStack(Material.IRON_INGOT),
-          new ItemStack(Material.IRON_INGOT), null, null, null, null, null, null});
-  public static final AbstractItemRecipe RECIPE_IRON_SWORD = new AbstractItemRecipe(new ItemStack(Material.IRON_SWORD, 1),
+          new ItemStack(Material.IRON_INGOT), null, null, null, null, null, null}, new ItemStack(Material.SHEARS, 1));
+  public static final AbstractItemRecipe RECIPE_IRON_SWORD = new AbstractItemRecipe(
       new ItemStack[]{new ItemStack(Material.STICK), new ItemStack(Material.IRON_INGOT),
-          new ItemStack(Material.IRON_INGOT), null, null, null, null, null, null});
-  public static final AbstractItemRecipe RECIPE_BARREL = new AbstractItemRecipe(new ItemStack(Material.BARREL, 1),
+          new ItemStack(Material.IRON_INGOT), null, null, null, null, null, null},
+      new ItemStack(Material.IRON_SWORD, 1));
+  public static final AbstractItemRecipe RECIPE_BARREL = new AbstractItemRecipe(
       new ItemStack[]{new ItemStack(Material.OAK_PLANKS), new ItemStack(Material.OAK_SLAB),
           new ItemStack(Material.OAK_PLANKS), new ItemStack(Material.OAK_PLANKS), null,
           new ItemStack(Material.OAK_PLANKS), new ItemStack(Material.OAK_PLANKS), new ItemStack(Material.OAK_SLAB),
-          new ItemStack(Material.OAK_PLANKS)});
-  public static final AbstractItemRecipe RECIPE_SYNTHETIC_AMETHYST = new AbstractItemRecipe(SupremeComponents.SYNTHETIC_AMETHYST,
-      SupremeComponents.RECIPE_SYNTHETIC_AMETHYST);
-  public static final AbstractItemRecipe RECIPE_SYNTHETIC_RUBY = new AbstractItemRecipe(SupremeComponents.SYNTHETIC_RUBY,
-      SupremeComponents.RECIPE_SYNTHETIC_RUBY);
+          new ItemStack(Material.OAK_PLANKS)}, new ItemStack(Material.BARREL, 1));
+  public static final AbstractItemRecipe RECIPE_SYNTHETIC_AMETHYST = new AbstractItemRecipe(
+      SupremeComponents.RECIPE_SYNTHETIC_AMETHYST, SupremeComponents.SYNTHETIC_AMETHYST);
+  public static final AbstractItemRecipe RECIPE_SYNTHETIC_RUBY = new AbstractItemRecipe(
+      SupremeComponents.RECIPE_SYNTHETIC_RUBY, SupremeComponents.SYNTHETIC_RUBY);
 
   public ElectricCrafter(ItemGroup category, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
     super(category, item, recipeType, recipe);
--- src/main/java/com/github/relativobr/supreme/machine/multiblock/ElectricGearFabricator.java
+++ src/main/java/com/github/relativobr/supreme/machine/multiblock/ElectricGearFabricator.java
@@ -1,6 +1,6 @@
 package com.github.relativobr.supreme.machine.multiblock;
 
-import com.github.relativobr.machine.MediumContainerMachine;
+import com.github.relativobr.machine.FlexItemContainerMachine;
 import com.github.relativobr.recipe.AbstractItemRecipe;
 import com.github.relativobr.supreme.gear.ArmorBasic;
 import com.github.relativobr.supreme.gear.ArmorThornium;
@@ -26,10 +26,10 @@ import javax.annotation.ParametersAreNonnullByDefault;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
-public class ElectricGearFabricator extends MediumContainerMachine {
+public class ElectricGearFabricator extends FlexItemContainerMachine {
 
   public static final SlimefunItemStack ELECTRIC_GEAR_MACHINE = new SupremeItemStack("SUPREME_ELECTRIC_GEAR_I",
-      Material.SMITHING_TABLE, "&bElectric Gear Machine", "", "&fCraft resource of gear", "",
+      Material.SMITHING_TABLE, "&bElectric Gear Machine", "", "&fCraft tier of gear", "",
       LoreBuilder.machine(MachineTier.ADVANCED, MachineType.MACHINE), LoreBuilder.speed(1),
       UtilEnergy.energyPowerPerSecond(20), "", "&3Supreme Machine");
 
@@ -39,7 +39,7 @@ public class ElectricGearFabricator extends MediumContainerMachine {
       SlimefunItems.ELECTRIC_MOTOR, SlimefunItems.CARBONADO_EDGED_CAPACITOR, SlimefunItems.ELECTRIC_MOTOR};
 
   public static final SlimefunItemStack ELECTRIC_GEAR_MACHINE_II = new SupremeItemStack("SUPREME_ELECTRIC_GEAR_II",
-      Material.SMITHING_TABLE, "&bElectric Gear Machine II", "", "&fAdvanced craft resource of gear", "",
+      Material.SMITHING_TABLE, "&bElectric Gear Machine II", "", "&fAdvanced craft tier of gear", "",
       LoreBuilder.machine(MachineTier.END_GAME, MachineType.MACHINE), LoreBuilder.speed(5),
       UtilEnergy.energyPowerPerSecond(100), "", "&3Supreme Machine");
 
@@ -50,7 +50,7 @@ public class ElectricGearFabricator extends MediumContainerMachine {
       SupremeComponents.BLEND_MACHINE};
 
   public static final SlimefunItemStack ELECTRIC_GEAR_MACHINE_III = new SupremeItemStack("SUPREME_ELECTRIC_GEAR_III",
-      Material.SMITHING_TABLE, "&bElectric Gear Machine III", "", "&fAdvanced craft resource of gear", "",
+      Material.SMITHING_TABLE, "&bElectric Gear Machine III", "", "&fAdvanced craft tier of gear", "",
       LoreBuilder.machine(MachineTier.END_GAME, MachineType.MACHINE), LoreBuilder.speed(15),
       UtilEnergy.energyPowerPerSecond(300), "", "&3Supreme Machine");
 
@@ -66,10 +66,10 @@ public class ElectricGearFabricator extends MediumContainerMachine {
 
   public static List<AbstractItemRecipe> getAllRecipe() {
     List<AbstractItemRecipe> list = new ArrayList<>();
-    list.add(new AbstractItemRecipe(ArmorBasic.RECIPE_TITANIUM_HELMET, ArmorBasic.TITANIUM_HELMET));
-    list.add(new AbstractItemRecipe(ArmorBasic.RECIPE_AURUM_HELMET, ArmorBasic.AURUM_HELMET));
-    list.add(new AbstractItemRecipe(ArmorBasic.RECIPE_ADAMANTIUM_HELMET, ArmorBasic.ADAMANTIUM_HELMET));
-    list.add(new AbstractItemRecipe(ArmorThornium.RECIPE_THORNIUM_HELMET, ArmorThornium.THORNIUM_HELMET));
+//    list.add(new AbstractItemRecipe(ArmorBasic.RECIPE_TITANIUM_HELMET, ArmorBasic.TITANIUM_HELMET));
+//    list.add(new AbstractItemRecipe(ArmorBasic.RECIPE_AURUM_HELMET, ArmorBasic.AURUM_HELMET));
+//    list.add(new AbstractItemRecipe(ArmorBasic.RECIPE_ADAMANTIUM_HELMET, ArmorBasic.ADAMANTIUM_HELMET));
+//    list.add(new AbstractItemRecipe(ArmorThornium.RECIPE_THORNIUM_HELMET, ArmorThornium.THORNIUM_HELMET));
     list.add(
         new AbstractItemRecipe(ItemTier.getMagicRecipe(ArmorThornium.THORNIUM_HELMET), ArmorThornium.THORNIUM_HELMET_MAGIC));
     list.add(new AbstractItemRecipe(ItemTier.getRareRecipe(ArmorThornium.THORNIUM_HELMET_MAGIC),
@@ -80,10 +80,10 @@ public class ElectricGearFabricator extends MediumContainerMachine {
         ArmorThornium.THORNIUM_HELMET_LEGENDARY));
     list.add(new AbstractItemRecipe(ItemTier.getSupremeRecipe(ArmorThornium.THORNIUM_HELMET_LEGENDARY),
         ArmorThornium.THORNIUM_HELMET_SUPREME));
-    list.add(new AbstractItemRecipe(ArmorBasic.RECIPE_TITANIUM_CHESTPLATE, ArmorBasic.TITANIUM_CHESTPLATE));
-    list.add(new AbstractItemRecipe(ArmorBasic.RECIPE_AURUM_CHESTPLATE, ArmorBasic.AURUM_CHESTPLATE));
-    list.add(new AbstractItemRecipe(ArmorBasic.RECIPE_ADAMANTIUM_CHESTPLATE, ArmorBasic.ADAMANTIUM_CHESTPLATE));
-    list.add(new AbstractItemRecipe(ArmorThornium.RECIPE_THORNIUM_CHESTPLATE, ArmorThornium.THORNIUM_CHESTPLATE));
+//    list.add(new AbstractItemRecipe(ArmorBasic.RECIPE_TITANIUM_CHESTPLATE, ArmorBasic.TITANIUM_CHESTPLATE));
+//    list.add(new AbstractItemRecipe(ArmorBasic.RECIPE_AURUM_CHESTPLATE, ArmorBasic.AURUM_CHESTPLATE));
+//    list.add(new AbstractItemRecipe(ArmorBasic.RECIPE_ADAMANTIUM_CHESTPLATE, ArmorBasic.ADAMANTIUM_CHESTPLATE));
+//    list.add(new AbstractItemRecipe(ArmorThornium.RECIPE_THORNIUM_CHESTPLATE, ArmorThornium.THORNIUM_CHESTPLATE));
     list.add(new AbstractItemRecipe(ItemTier.getMagicRecipe(ArmorThornium.THORNIUM_CHESTPLATE),
         ArmorThornium.THORNIUM_CHESTPLATE_MAGIC));
     list.add(new AbstractItemRecipe(ItemTier.getRareRecipe(ArmorThornium.THORNIUM_CHESTPLATE_MAGIC),
@@ -94,10 +94,10 @@ public class ElectricGearFabricator extends MediumContainerMachine {
         ArmorThornium.THORNIUM_CHESTPLATE_LEGENDARY));
     list.add(new AbstractItemRecipe(ItemTier.getSupremeRecipe(ArmorThornium.THORNIUM_CHESTPLATE_LEGENDARY),
         ArmorThornium.THORNIUM_CHESTPLATE_SUPREME));
-    list.add(new AbstractItemRecipe(ArmorBasic.RECIPE_TITANIUM_LEGGINGS, ArmorBasic.TITANIUM_LEGGINGS));
-    list.add(new AbstractItemRecipe(ArmorBasic.RECIPE_AURUM_LEGGINGS, ArmorBasic.AURUM_LEGGINGS));
-    list.add(new AbstractItemRecipe(ArmorBasic.RECIPE_ADAMANTIUM_LEGGINGS, ArmorBasic.ADAMANTIUM_LEGGINGS));
-    list.add(new AbstractItemRecipe(ArmorThornium.RECIPE_THORNIUM_LEGGINGS, ArmorThornium.THORNIUM_LEGGINGS));
+//    list.add(new AbstractItemRecipe(ArmorBasic.RECIPE_TITANIUM_LEGGINGS, ArmorBasic.TITANIUM_LEGGINGS));
+//    list.add(new AbstractItemRecipe(ArmorBasic.RECIPE_AURUM_LEGGINGS, ArmorBasic.AURUM_LEGGINGS));
+//    list.add(new AbstractItemRecipe(ArmorBasic.RECIPE_ADAMANTIUM_LEGGINGS, ArmorBasic.ADAMANTIUM_LEGGINGS));
+//    list.add(new AbstractItemRecipe(ArmorThornium.RECIPE_THORNIUM_LEGGINGS, ArmorThornium.THORNIUM_LEGGINGS));
     list.add(new AbstractItemRecipe(ItemTier.getMagicRecipe(ArmorThornium.THORNIUM_LEGGINGS),
         ArmorThornium.THORNIUM_LEGGINGS_MAGIC));
     list.add(new AbstractItemRecipe(ItemTier.getRareRecipe(ArmorThornium.THORNIUM_LEGGINGS_MAGIC),
@@ -108,10 +108,10 @@ public class ElectricGearFabricator extends MediumContainerMachine {
         ArmorThornium.THORNIUM_LEGGINGS_LEGENDARY));
     list.add(new AbstractItemRecipe(ItemTier.getSupremeRecipe(ArmorThornium.THORNIUM_LEGGINGS_LEGENDARY),
         ArmorThornium.THORNIUM_LEGGINGS_SUPREME));
-    list.add(new AbstractItemRecipe(ArmorBasic.RECIPE_TITANIUM_BOOTS, ArmorBasic.TITANIUM_BOOTS));
-    list.add(new AbstractItemRecipe(ArmorBasic.RECIPE_AURUM_BOOTS, ArmorBasic.AURUM_BOOTS));
-    list.add(new AbstractItemRecipe(ArmorBasic.RECIPE_ADAMANTIUM_BOOTS, ArmorBasic.ADAMANTIUM_BOOTS));
-    list.add(new AbstractItemRecipe(ArmorThornium.RECIPE_THORNIUM_BOOTS, ArmorThornium.THORNIUM_BOOTS));
+//    list.add(new AbstractItemRecipe(ArmorBasic.RECIPE_TITANIUM_BOOTS, ArmorBasic.TITANIUM_BOOTS));
+//    list.add(new AbstractItemRecipe(ArmorBasic.RECIPE_AURUM_BOOTS, ArmorBasic.AURUM_BOOTS));
+//    list.add(new AbstractItemRecipe(ArmorBasic.RECIPE_ADAMANTIUM_BOOTS, ArmorBasic.ADAMANTIUM_BOOTS));
+//    list.add(new AbstractItemRecipe(ArmorThornium.RECIPE_THORNIUM_BOOTS, ArmorThornium.THORNIUM_BOOTS));
     list.add(
         new AbstractItemRecipe(ItemTier.getMagicRecipe(ArmorThornium.THORNIUM_BOOTS), ArmorThornium.THORNIUM_BOOTS_MAGIC));
     list.add(new AbstractItemRecipe(ItemTier.getRareRecipe(ArmorThornium.THORNIUM_BOOTS_MAGIC),
@@ -122,7 +122,7 @@ public class ElectricGearFabricator extends MediumContainerMachine {
         ArmorThornium.THORNIUM_BOOTS_LEGENDARY));
     list.add(new AbstractItemRecipe(ItemTier.getSupremeRecipe(ArmorThornium.THORNIUM_BOOTS_LEGENDARY),
         ArmorThornium.THORNIUM_BOOTS_SUPREME));
-    list.add(new AbstractItemRecipe(WeaponsThornium.RECIPE_THORNIUM_BOW, WeaponsThornium.THORNIUM_BOW));
+//    list.add(new AbstractItemRecipe(WeaponsThornium.RECIPE_THORNIUM_BOW, WeaponsThornium.THORNIUM_BOW));
     list.add(
         new AbstractItemRecipe(ItemTier.getMagicRecipe(WeaponsThornium.THORNIUM_BOW), WeaponsThornium.THORNIUM_BOW_MAGIC));
     list.add(new AbstractItemRecipe(ItemTier.getRareRecipe(WeaponsThornium.THORNIUM_BOW_MAGIC),
@@ -133,10 +133,10 @@ public class ElectricGearFabricator extends MediumContainerMachine {
         WeaponsThornium.THORNIUM_BOW_LEGENDARY));
     list.add(new AbstractItemRecipe(ItemTier.getSupremeRecipe(WeaponsThornium.THORNIUM_BOW_LEGENDARY),
         WeaponsThornium.THORNIUM_BOW_SUPREME));
-    list.add(new AbstractItemRecipe(WeaponsBasic.RECIPE_TITANIUM_SWORD, WeaponsBasic.TITANIUM_SWORD));
-    list.add(new AbstractItemRecipe(WeaponsBasic.RECIPE_AURUM_SWORD, WeaponsBasic.AURUM_SWORD));
-    list.add(new AbstractItemRecipe(WeaponsBasic.RECIPE_ADAMANTIUM_SWORD, WeaponsBasic.ADAMANTIUM_SWORD));
-    list.add(new AbstractItemRecipe(WeaponsThornium.RECIPE_THORNIUM_SWORD, WeaponsThornium.THORNIUM_SWORD));
+//    list.add(new AbstractItemRecipe(WeaponsBasic.RECIPE_TITANIUM_SWORD, WeaponsBasic.TITANIUM_SWORD));
+//    list.add(new AbstractItemRecipe(WeaponsBasic.RECIPE_AURUM_SWORD, WeaponsBasic.AURUM_SWORD));
+//    list.add(new AbstractItemRecipe(WeaponsBasic.RECIPE_ADAMANTIUM_SWORD, WeaponsBasic.ADAMANTIUM_SWORD));
+//    list.add(new AbstractItemRecipe(WeaponsThornium.RECIPE_THORNIUM_SWORD, WeaponsThornium.THORNIUM_SWORD));
     list.add(new AbstractItemRecipe(ItemTier.getMagicRecipe(WeaponsThornium.THORNIUM_SWORD),
         WeaponsThornium.THORNIUM_SWORD_MAGIC));
     list.add(new AbstractItemRecipe(ItemTier.getRareRecipe(WeaponsThornium.THORNIUM_SWORD_MAGIC),
@@ -147,7 +147,7 @@ public class ElectricGearFabricator extends MediumContainerMachine {
         WeaponsThornium.THORNIUM_SWORD_LEGENDARY));
     list.add(new AbstractItemRecipe(ItemTier.getSupremeRecipe(WeaponsThornium.THORNIUM_SWORD_LEGENDARY),
         WeaponsThornium.THORNIUM_SWORD_SUPREME));
-    list.add(new AbstractItemRecipe(WeaponsThornium.RECIPE_THORNIUM_SHIELD, WeaponsThornium.THORNIUM_SHIELD));
+//    list.add(new AbstractItemRecipe(WeaponsThornium.RECIPE_THORNIUM_SHIELD, WeaponsThornium.THORNIUM_SHIELD));
     list.add(new AbstractItemRecipe(ItemTier.getMagicRecipe(WeaponsThornium.THORNIUM_SHIELD),
         WeaponsThornium.THORNIUM_SHIELD_MAGIC));
     list.add(new AbstractItemRecipe(ItemTier.getRareRecipe(WeaponsThornium.THORNIUM_SHIELD_MAGIC),
@@ -158,10 +158,10 @@ public class ElectricGearFabricator extends MediumContainerMachine {
         WeaponsThornium.THORNIUM_SHIELD_LEGENDARY));
     list.add(new AbstractItemRecipe(ItemTier.getSupremeRecipe(WeaponsThornium.THORNIUM_SHIELD_LEGENDARY),
         WeaponsThornium.THORNIUM_SHIELD_SUPREME));
-    list.add(new AbstractItemRecipe(ToolsBasic.RECIPE_TITANIUM_AXE, ToolsBasic.TITANIUM_AXE));
-    list.add(new AbstractItemRecipe(ToolsBasic.RECIPE_AURUM_AXE, ToolsBasic.AURUM_AXE));
-    list.add(new AbstractItemRecipe(ToolsBasic.RECIPE_ADAMANTIUM_AXE, ToolsBasic.ADAMANTIUM_AXE));
-    list.add(new AbstractItemRecipe(ToolsThornium.RECIPE_THORNIUM_AXE, ToolsThornium.THORNIUM_AXE));
+//    list.add(new AbstractItemRecipe(ToolsBasic.RECIPE_TITANIUM_AXE, ToolsBasic.TITANIUM_AXE));
+//    list.add(new AbstractItemRecipe(ToolsBasic.RECIPE_AURUM_AXE, ToolsBasic.AURUM_AXE));
+//    list.add(new AbstractItemRecipe(ToolsBasic.RECIPE_ADAMANTIUM_AXE, ToolsBasic.ADAMANTIUM_AXE));
+//    list.add(new AbstractItemRecipe(ToolsThornium.RECIPE_THORNIUM_AXE, ToolsThornium.THORNIUM_AXE));
     list.add(new AbstractItemRecipe(ItemTier.getMagicRecipe(ToolsThornium.THORNIUM_AXE), ToolsThornium.THORNIUM_AXE_MAGIC));
     list.add(
         new AbstractItemRecipe(ItemTier.getRareRecipe(ToolsThornium.THORNIUM_AXE_MAGIC), ToolsThornium.THORNIUM_AXE_RARE));
@@ -171,10 +171,10 @@ public class ElectricGearFabricator extends MediumContainerMachine {
         ToolsThornium.THORNIUM_AXE_LEGENDARY));
     list.add(new AbstractItemRecipe(ItemTier.getSupremeRecipe(ToolsThornium.THORNIUM_AXE_LEGENDARY),
         ToolsThornium.THORNIUM_AXE_SUPREME));
-    list.add(new AbstractItemRecipe(ToolsBasic.RECIPE_TITANIUM_PICKAXE, ToolsBasic.TITANIUM_PICKAXE));
-    list.add(new AbstractItemRecipe(ToolsBasic.RECIPE_AURUM_PICKAXE, ToolsBasic.AURUM_PICKAXE));
-    list.add(new AbstractItemRecipe(ToolsBasic.RECIPE_ADAMANTIUM_PICKAXE, ToolsBasic.ADAMANTIUM_PICKAXE));
-    list.add(new AbstractItemRecipe(ToolsThornium.RECIPE_THORNIUM_PICKAXE, ToolsThornium.THORNIUM_PICKAXE));
+//    list.add(new AbstractItemRecipe(ToolsBasic.RECIPE_TITANIUM_PICKAXE, ToolsBasic.TITANIUM_PICKAXE));
+//    list.add(new AbstractItemRecipe(ToolsBasic.RECIPE_AURUM_PICKAXE, ToolsBasic.AURUM_PICKAXE));
+//    list.add(new AbstractItemRecipe(ToolsBasic.RECIPE_ADAMANTIUM_PICKAXE, ToolsBasic.ADAMANTIUM_PICKAXE));
+//    list.add(new AbstractItemRecipe(ToolsThornium.RECIPE_THORNIUM_PICKAXE, ToolsThornium.THORNIUM_PICKAXE));
     list.add(new AbstractItemRecipe(ItemTier.getMagicRecipe(ToolsThornium.THORNIUM_PICKAXE),
         ToolsThornium.THORNIUM_PICKAXE_MAGIC));
     list.add(new AbstractItemRecipe(ItemTier.getRareRecipe(ToolsThornium.THORNIUM_PICKAXE_MAGIC),
@@ -185,10 +185,10 @@ public class ElectricGearFabricator extends MediumContainerMachine {
         ToolsThornium.THORNIUM_PICKAXE_LEGENDARY));
     list.add(new AbstractItemRecipe(ItemTier.getSupremeRecipe(ToolsThornium.THORNIUM_PICKAXE_LEGENDARY),
         ToolsThornium.THORNIUM_PICKAXE_SUPREME));
-    list.add(new AbstractItemRecipe(ToolsBasic.RECIPE_TITANIUM_SHOVEL, ToolsBasic.TITANIUM_SHOVEL));
-    list.add(new AbstractItemRecipe(ToolsBasic.RECIPE_AURUM_SHOVEL, ToolsBasic.AURUM_SHOVEL));
-    list.add(new AbstractItemRecipe(ToolsBasic.RECIPE_ADAMANTIUM_SHOVEL, ToolsBasic.ADAMANTIUM_SHOVEL));
-    list.add(new AbstractItemRecipe(ToolsThornium.RECIPE_THORNIUM_SHOVEL, ToolsThornium.THORNIUM_SHOVEL));
+//    list.add(new AbstractItemRecipe(ToolsBasic.RECIPE_TITANIUM_SHOVEL, ToolsBasic.TITANIUM_SHOVEL));
+//    list.add(new AbstractItemRecipe(ToolsBasic.RECIPE_AURUM_SHOVEL, ToolsBasic.AURUM_SHOVEL));
+//    list.add(new AbstractItemRecipe(ToolsBasic.RECIPE_ADAMANTIUM_SHOVEL, ToolsBasic.ADAMANTIUM_SHOVEL));
+//    list.add(new AbstractItemRecipe(ToolsThornium.RECIPE_THORNIUM_SHOVEL, ToolsThornium.THORNIUM_SHOVEL));
     list.add(
         new AbstractItemRecipe(ItemTier.getMagicRecipe(ToolsThornium.THORNIUM_SHOVEL), ToolsThornium.THORNIUM_SHOVEL_MAGIC));
     list.add(new AbstractItemRecipe(ItemTier.getRareRecipe(ToolsThornium.THORNIUM_SHOVEL_MAGIC),
