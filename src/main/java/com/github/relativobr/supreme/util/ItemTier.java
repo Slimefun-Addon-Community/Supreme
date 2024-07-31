b08b70ba47d6be5a7f9699bb1ba672b11fc4135f
ybw0014 <i@guizhanss.com>
2024-07-31 15:22:17 -0400

    fix: paper 1.21 compatibility

--- src/main/java/com/github/relativobr/supreme/util/ItemTier.java
+++ src/main/java/com/github/relativobr/supreme/util/ItemTier.java
@@ -9,7 +9,7 @@ public class ItemTier {
 
   public static ItemStack[] getMagicRecipe(SlimefunItemStack preItem) {
     return new ItemStack[]{SupremeComponents.THORNIUM_BIT_SYNTHETIC, preItem, SupremeComponents.THORNIUM_BIT_SYNTHETIC,
-        SupremeComponents.THORNIUM_BIT_SYNTHETIC, new ItemStack(SupremeCetrus.CETRUS_IGNIS),
+        SupremeComponents.THORNIUM_BIT_SYNTHETIC, SupremeCetrus.CETRUS_IGNIS,
         SupremeComponents.THORNIUM_BIT_SYNTHETIC, SupremeComponents.THORNIUM_BIT_SYNTHETIC, preItem,
         SupremeComponents.THORNIUM_BIT_SYNTHETIC};
   }
@@ -17,20 +17,20 @@ public class ItemTier {
   public static ItemStack[] getRareRecipe(SlimefunItemStack preItem) {
     return new ItemStack[]{SupremeComponents.THORNIUM_DUST_SYNTHETIC, preItem,
         SupremeComponents.THORNIUM_DUST_SYNTHETIC, SupremeComponents.THORNIUM_DUST_SYNTHETIC,
-        new ItemStack(SupremeCetrus.CETRUS_VENTUS), SupremeComponents.THORNIUM_DUST_SYNTHETIC,
+        SupremeCetrus.CETRUS_VENTUS, SupremeComponents.THORNIUM_DUST_SYNTHETIC,
         SupremeComponents.THORNIUM_DUST_SYNTHETIC, preItem, SupremeComponents.THORNIUM_DUST_SYNTHETIC};
   }
 
   public static ItemStack[] getEpicRecipe(SlimefunItemStack preItem) {
     return new ItemStack[]{SupremeComponents.THORNIUM_INGOT_SYNTHETIC, preItem,
         SupremeComponents.THORNIUM_INGOT_SYNTHETIC, SupremeComponents.THORNIUM_INGOT_SYNTHETIC,
-        new ItemStack(SupremeCetrus.CETRUS_LUX), SupremeComponents.THORNIUM_INGOT_SYNTHETIC,
+        SupremeCetrus.CETRUS_LUX, SupremeComponents.THORNIUM_INGOT_SYNTHETIC,
         SupremeComponents.THORNIUM_INGOT_SYNTHETIC, preItem, SupremeComponents.THORNIUM_INGOT_SYNTHETIC};
   }
 
   public static ItemStack[] getLegendaryRecipe(SlimefunItemStack preItem) {
     return new ItemStack[]{SupremeComponents.THORNIUM_CARBONADO, preItem, SupremeComponents.THORNIUM_CARBONADO,
-        SupremeComponents.THORNIUM_CARBONADO, new ItemStack(SupremeCetrus.CETRUS_LUMIUM),
+        SupremeComponents.THORNIUM_CARBONADO, SupremeCetrus.CETRUS_LUMIUM,
         SupremeComponents.THORNIUM_CARBONADO, SupremeComponents.THORNIUM_CARBONADO, preItem,
         SupremeComponents.THORNIUM_CARBONADO};
   }
